790ad0150172b938d86395e6c5f85e55226d42f1
GFerris <gferris@bgsu.edu>
2024-12-22 15:49:14 -0500

    Added basic html

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Grant Ferris</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body class="bg-light">
+    <navbar class="navbar navbar-dark" style="background-color:black;">
+        <div class="container">
+            <div class="navbar-header">
+                <a href="index.php" class="navbar-brand">Grant Ferris</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </div>
+    </navbar>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="text-center">Grant Ferris</h1>
+                <p class="text-center">Welcome to my website!</p>
+            </div>
+        </div>
+</div>
+</body>
